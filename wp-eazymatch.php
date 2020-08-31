6307d0af51bf4b9656dee6426d0340373f17b499
Vincent van den Nieuwenhuizen <vincent@inforvision.nl>
2020-08-31 12:07:19 +0200

    Update wp-eazymatch.php

--- wp-eazymatch.php
+++ wp-eazymatch.php
@@ -4,13 +4,13 @@ Plugin Name: EazyMatch
 Plugin URI: https://github.com/viancen/wp-eazymatch
 Description: De EazyMatch Wordpress plugin. Bij twijfel over de instellingen mail naar support@eazymatch.nl
 Name: EazyMatch
-Version: 6.1.4
+Version: 6.1.5
 Author: EazyMatch
 Author URI: https://eazymatch-online.nl
 */
 
 #php
-$globalVersion = '6.1.4';
+$globalVersion = '6.1.5';
 define('EMOL_VERSION', $globalVersion);
 
 //eazymatch directory on server
