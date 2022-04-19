29daf0fbdc2f0eacf98e49bed9a7c219f7e797b9
Vincent <vincent@inforvision.nl>
2022-04-19 09:29:35 -0400

    Update factory.php

--- lib/emol/jobfilter/factory.php
+++ lib/emol/jobfilter/factory.php
@@ -1,34 +1,39 @@
 <?php
 
-class emol_jobfilter_factory {
-	public static function create( $filterName = 'default' ) {
-        return match ($filterName) {
-            'default' => emol_jobfilter_factory::createDefault(),
-            default => new emol_jobfilter_instance(),
-        };
-	}
+class emol_jobfilter_factory
+{
+    public static function create($filterName = 'default')
+    {
+        switch ($filterName) {
+            case 'default':
+                return emol_jobfilter_factory::createDefault();
+            default:
+                return new emol_jobfilter_instance();
+        }
+    }
 
-	public static function createDefault() {
-		$filter = get_option( 'emol_jobfilter_default', false );
+    public static function createDefault()
+    {
+        $filter = get_option('emol_jobfilter_default', false);
 
-		// if not filter is found, automatically add the filter
-		if ( $filter == false ) {
-			$filter = new emol_jobfilter_instance();
-			update_option( 'emol_jobfilter_default', $filter );
-		}
+        // if not filter is found, automatically add the filter
+        if ($filter == false) {
+            $filter = new emol_jobfilter_instance();
+            update_option('emol_jobfilter_default', $filter);
+        }
 
-		// transform legacy filter
-		$legacyStatus = get_option( 'emol_filter_options', false );
+        // transform legacy filter
+        $legacyStatus = get_option('emol_filter_options', false);
 
-		if ( $legacyStatus !== false ) {
-			$filter->setStatus( unserialize( $legacyStatus ) );
-			update_option( 'emol_jobfilter_default', $filter );
+        if ($legacyStatus !== false) {
+            $filter->setStatus(unserialize($legacyStatus));
+            update_option('emol_jobfilter_default', $filter);
 
-			// delete legacy option
-			delete_option( 'emol_filter_options' );
-		}
+            // delete legacy option
+            delete_option('emol_filter_options');
+        }
 
 
-		return $filter;
-	}
+        return $filter;
+    }
 }
